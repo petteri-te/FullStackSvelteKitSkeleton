679b2a88e915a554cde6d7776452cbeeaacd5079
Petteri-Teaching <petteri-teaching@Petteris-MacBook-Air.local>
2024-02-17 19:47:47 +0200

    added layout

---
+++ ui/src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/todo">Todo</a>
+</nav>
+
+<slot />
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,9 +1,6 @@
 
 
 <h1>Welcome to Petteri's full stack example</h1>
-<p>main page link to todo page  goto /todo</p>
+<p>Based on Aalto University's Web software development 2.0 course</p>
 
-<nav>
-    <a href="/">Home</a>
-    <a href="/todo">Todo</a>
-</nav>
+<a href="https://fitech101.aalto.fi/web-software-development-2-0">Link to course</a>
